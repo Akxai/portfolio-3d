b47f822db03deeb776a0609f2bd5512a916a392f
Akshay Aileni <akshayakshay5543@gmail.com>
2023-12-01 21:11:48 +0530

    fix:frameloop to frameLoop

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -15,7 +15,7 @@ const EarthCanvas = () => {
   return (
     <Canvas
       shadows
-      frameloop="demand"
+      frameLoop="demand"
       gl={{ preserveDrawingBuffer: true }}
       camera={{
         fov: 45,
